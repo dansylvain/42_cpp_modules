9d99096d8627b89145a3eda06e96cc670d5f6079
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-19 10:01:50 +0200

    all zombies created

--- C01/ex00/Zombie.cpp
+++ C01/ex00/Zombie.cpp
@@ -11,11 +11,30 @@ void Zombie::announce(void)
 	std::cout << this->name << ": " << this->mssg << std::endl;
 }
 
+Zombie* newZombie( std::string name )
+{
+	Zombie *zombie = new Zombie(name);
+	return (zombie);
+}
 
+void	randomChump( std::string name )
+{
+	Zombie tyrone_zombie(name);
+
+	tyrone_zombie.announce();
+
+}
+/**========================================================================
+ *                           MAIN
+ * Tyrone
+ *========================================================================**/
 int main(void)
 {
-	Zombie zombie("foo");
+	Zombie zombie("Bub");
 
+	Zombie *bar_zombie = newZombie("Zombina");
 	zombie.announce();
+	bar_zombie->announce();
+	randomChump("Tyrone");
 	return (0);
 }
--- C01/ex00/Zombie.hpp
+++ C01/ex00/Zombie.hpp
@@ -13,7 +13,9 @@ class Zombie {
 	public:
 		Zombie();
 		Zombie(const std::string& name);
-		void announce(void);
+		void	announce(void);
+		Zombie*	newZombie( std::string name );
+		void	randomChump( std::string name );
 };
 
 #endif
