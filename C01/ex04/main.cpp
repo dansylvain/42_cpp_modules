417d40147a6e4226d7e718d2f25518c533c9eade
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-19 17:17:59 +0200

    file opened and read

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -2,8 +2,37 @@
 #include <fstream>
 
 
-int main( void )
+int main( int argc, char **argv )
 {
-	std::cout << "Hello World!" << std::endl;
+	std::string line;
+
+	// arg number check
+	if (argc != 4)
+	{
+		std::cout << "Error: wrong number of arguments" << std::endl;
+		return (1);
+	}
+
+	// test1
+	std::cout << argv[1] << std::endl;
+
+
+	// open input file stream + check
+	std::ifstream inputFile(argv[1]);
+	if (!inputFile)
+	{
+		std::cout << "Error: file not found" << std::endl;
+		return (2);
+	}
+
+	while (std::getline(inputFile, line))
+	{
+		std::cout << line << std::endl;
+	}
+
+	// close input file stream
+	inputFile.close();
+
 	return (0);
+	(void)argc;
 }
