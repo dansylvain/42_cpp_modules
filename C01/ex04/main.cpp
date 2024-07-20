0007c4cc68e1b3eafcad390a9ecada9eae3e9528
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-20 09:53:02 +0200

    start refactoring

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -1,23 +1,19 @@
-#include <iostream>
-#include <fstream>
+#include "Utils.hpp"
 
 std::string replaceAllOccurrences(const std::string& str, const std::string& s1, const std::string& s2) {
     if (s1.empty()) {
-        return str; // Avoid infinite loop if s1 is empty
+        return str;
     }
-
     std::string result;
     size_t pos = 0;
     size_t prev_pos = 0;
-
     while ((pos = str.find(s1, prev_pos)) != std::string::npos) {
-		std::cout << "pos: " << pos << std::endl;
-        result.append(str.substr(prev_pos, pos - prev_pos)); // Append part before s1
-        result.append(s2); // Append s2 instead of s1
-        prev_pos = pos + s1.length(); // Move past the occurrence of s1
+        result.append(str.substr(prev_pos, pos - prev_pos));
+        result.append(s2);
+        prev_pos = pos + s1.length();
     }
 
-    result.append(str.substr(prev_pos)); // Append the remainder of the string
+    result.append(str.substr(prev_pos));
     return result;
 }
 
@@ -43,10 +39,10 @@ int main( int argc, char **argv )
 	s2 = argv[3];
 
 	// test1
-	std::cout << argv[1] << std::endl;
-	std::cout << destFile << std::endl;
-	std::cout << s1 << std::endl;
-	std::cout << s2 << std::endl;
+	// std::cout << argv[1] << std::endl;
+	// std::cout << destFile << std::endl;
+	// std::cout << s1 << std::endl;
+	// std::cout << s2 << std::endl;
 
 
 	// open input file stream + check
@@ -68,7 +64,9 @@ int main( int argc, char **argv )
 	std::string newline;
 	while (std::getline(inputFile, line))
 	{
+		std::cout << line << std::endl;
 		newline = replaceAllOccurrences(line, s1, s2);
+		std::cout << newline << std::endl;
 		outputFile << newline << std::endl;
 	}
 
@@ -78,6 +76,7 @@ int main( int argc, char **argv )
 	// close input file stream
 	inputFile.close();
 
+	
 	return (0);
 	(void)argc;
 }
