3de10f4149ed0ca9409867a4559482b4d493fa6d
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-23 16:46:06 +0200

    exo 1 seems fine

---
+++ C02/ex00/Fixed.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.hpp"
+
+
+Fixed::Fixed() : _value(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed) : _value(fixed._value) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fixed;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed){
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &fixed) {
+		this->_value = fixed.getRawBits();
+	}
+	return *this;
+
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits(void) const{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_value;
+
+}
+
+void	Fixed::setRawBits(int const raw){
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+
+}
