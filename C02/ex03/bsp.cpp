6ce342b2200698886a8ce065478208d54e6e867c
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-26 11:49:53 +0200

    func seems to be working

--- C02/ex03/bsp.cpp
+++ C02/ex03/bsp.cpp
@@ -1,6 +1,6 @@
 #include "Point.hpp"
 #include "Fixed.hpp"
-
+#include <cstdio>
 
 /**========================================================================
  *                             BSP
@@ -16,12 +16,13 @@ int	cross_product_2d(int x[], int y[], int z[]){
 
 void	initializeArray(int array[], Point const p)
 {
-	array[0] = p.get_x().getRawBits();
-	array[1] = p.get_y().getRawBits();
+	array[0] = p.get_x().toInt();
+	array[1] = p.get_y().toInt();
 }
 
 bool haveSameSign(int x, int y)
 {
+	printf("x: %i, y: %i\n", x, y);
 	if ((x > 0 && y > 0) || (x < 0 && y < 0))
 		return (true);
 	return (false);
@@ -34,11 +35,17 @@ bool bsp( Point const a, Point const b, Point const c, Point const point){
 	initializeArray(C, c);
 	initializeArray(P, point);
 
-	bool sameSign1 = haveSameSign(cross_product_2d(A, B, C), cross_product_2d(A, B, P));
-	bool sameSign2 = haveSameSign(cross_product_2d(A, C, B), cross_product_2d(A, C, P));
-	bool sameSign3 = haveSameSign(cross_product_2d(B, C, A), cross_product_2d(B, C, P));
-	
-	std::cout << sameSign1 << ", " << sameSign2 << ", " << sameSign3 << std::endl;
-	
+	int cp1 = cross_product_2d(A, B, C);
+	int cp2 = cross_product_2d(A, B, P);
+	int cp3 = cross_product_2d(B, C, P);
+	int cp4 = cross_product_2d(C, A, P);
+
+	bool sameSign1 = haveSameSign(cp1, cp2);
+	bool sameSign2 = haveSameSign(cp1, cp3);
+	bool sameSign3 = haveSameSign(cp1, cp4);
+
+	std::cout << "Cross Products: " << cp1 << ", " << cp2 << ", " << cp3 << ", " << cp4 << std::endl;
+	std::cout << "Same Signs: " << sameSign1 << ", " << sameSign2 << ", " << sameSign3 << std::endl;
+
 	return (sameSign1 && sameSign2 && sameSign3);
 }
