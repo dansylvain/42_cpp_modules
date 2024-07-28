cbe71e5c98a735beb84ef1782e4f2f6f8d346744
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-28 09:48:17 +0200

    diamondTrap.hpp written

---
+++ C03/ex03/DiamondTrap.hpp
@@ -0,0 +1,28 @@
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"	
+#include "FragTrap.hpp"
+
+class DiamondTrap {
+	private:
+		std::string			_Name;
+		int					_HitPoints;
+		int					_EnergyPoints;
+		int					_AttackDamage;
+	public:
+		// Constructors and destructor
+		DiamondTrap();
+		DiamondTrap(const std::string& name);
+		DiamondTrap(const DiamondTrap &other);
+		~DiamondTrap();
+
+		// assignment operator
+		DiamondTrap &operator=(const DiamondTrap &src);
+
+		// other methods
+		void whoAmI();
+};
+
+#endif
