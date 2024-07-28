c0ab62d628180ffba40ef1248cfdfb03e070669f
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-28 07:09:26 +0200

    files FragTrap.c and FragTrap.hpp created

---
+++ C03/ex02/FragTrap.cpp
@@ -0,0 +1,38 @@
+#include "FragTrap.hpp"
+
+/**========================================================================
+ *                           CONSTRUCTORS AND DESTRUCTOR
+ *========================================================================**/
+FragTrap::FragTrap() : ClapTrap() {
+	DisplayName(" created***");};
+
+FragTrap::FragTrap(const std::string& name) : ClapTrap(name) {
+	DisplayName(" created***");};
+
+FragTrap::FragTrap(const FragTrap &other) : ClapTrap(other) {
+	DisplayName(" created***");};
+
+FragTrap::~FragTrap() {
+	DisplayName(" destroyed***");};
+
+/**========================================================================
+ *                          ASSIGNMENT OPERATOR
+ *========================================================================**/
+FragTrap& FragTrap::operator=(const FragTrap& src) {
+	if (this == &src)
+		return *this;
+	ClapTrap::operator=(src);
+	return *this;
+}
+
+/**========================================================================
+ *                           ACTION METHODS
+ *========================================================================**/
+void FragTrap::attack(const std::string& target) {
+	std::cout << "***ScrapTrap*** ";
+	ClapTrap::attack(target);
+}
+
+void FragTrap::guardGate() {
+	print("***ScrapTrap*** ", this->getName(), " Enter Gate Keeper Mode");
+};
---
+++ C03/ex02/FragTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+	public:
+		// Constructors and destructor
+		FragTrap();
+		FragTrap(const std::string& name);
+		FragTrap(const FragTrap &other);
+		~FragTrap();
+
+		// assignment operator
+	    FragTrap& operator=(const FragTrap& src);
+
+		// other methods
+		void attack(const std::string& target);
+		void guardGate();
+};
+
+#endif
