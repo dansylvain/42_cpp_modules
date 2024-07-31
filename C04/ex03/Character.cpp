2f9b0c78b66d938d534868b87dd093a5fdb47d59
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-31 11:12:23 +0200

    first writing session done

--- C04/ex03/Character.cpp
+++ C04/ex03/Character.cpp
@@ -1,10 +1,36 @@
 #include "Character.hpp"
 
 
+Character::Character() : ICharacter()
+{
+
+}
+
+Character::Character(std::string const & name) : ICharacter(), name(name)
+{
+
+}
+
+Character::Character(const Character& other) : ICharacter(), name(other.name)
+{
+
+}
+
 Character::~Character()
 {
-	
+
 }
+
+Character& Character::operator=(const Character& other)
+{
+	if (this != &other)
+	{
+		ICharacter::operator=(other);
+		name = other.name;
+	}
+	return (*this);
+}
+
 std::string const & Character::getName() const
 {
 	return (name);
