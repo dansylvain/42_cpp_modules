3f2a4d75d315443dd8d78cc6357c59f459bc784f
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-31 08:58:57 +0200

    hpp files for ice and cure written

---
+++ C04/ex03/Cure.hpp
@@ -0,0 +1,19 @@
+#ifndef CURE_HPP
+#define CURE_HPP
+
+#include "AMateria.hpp"
+
+class Cure : AMateria {
+	// constructeur par défaut
+	Cure(std::string const & type);
+	// constructeur par copie
+	Cure(const Cure &other);
+	// operateur d'assignation
+	Cure& operator=(const Cure& other);
+	// destructeur
+	~Cure();
+	AMateria* clone() const;
+	void use(ICharacter& target);
+};
+
+#endif
