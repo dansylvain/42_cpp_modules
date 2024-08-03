abdefbc418aca3234729b8dc93a03cb189ba3e9f
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-03 09:42:40 +0200

    implemented window opening

---
+++ C04/ex03/exec.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "AMateria.hpp"
+#include "Character.hpp"
+#include "Cure.hpp"
+#include "Ice.hpp"
+#include "MateriaSource.hpp"
+
+void	print(std::string str);
+void	printB(const std::string& str);
+void	printF(std::string str);
+void	test_one();
+void	test_two();
+
+bool TEST;
+
+
+
+
+int main()
+{
+	TEST = true;
+	// std::string c;
+	// while (c != "1" && c != "2")
+	// {
+	// 	std::cout << "test 1 or 2? ";
+	// 	std::cin >> c; 
+	// }
+	// switch (c[0])
+	// {
+	// 	case '1':
+	// 		test_one();
+	// 		break;
+	// 	case '2' :
+	// 		test_two();
+	// 		break;
+	// 	default :
+	// 		print("Fuck off");
+	// 		break;
+	// }
+	test_two();
+	return 0;
+}
+
+void	print(std::string str)
+{
+	if (TEST)
+		std::cout << str << std::endl;
+}
+
+void printB(const std::string& str)
+{
+    // Code d'échappement pour le texte en gras (1) et couleur verte (32)
+	if (TEST)
+    	std::cout << "\033[1;32m" << str << "\033[0m" << std::endl;
+}
+
+void	printF(std::string str)
+{
+		std::cout << str << std::endl;
+}
--- C04/ex03/main.cpp
+++ C04/ex03/main.cpp
@@ -1,60 +1,26 @@
+#include "Terminal.hpp"
 #include <iostream>
-#include "AMateria.hpp"
-#include "Character.hpp"
-#include "Cure.hpp"
-#include "Ice.hpp"
-#include "MateriaSource.hpp"
-
-void	print(std::string str);
-void	printB(const std::string& str);
-void	printF(std::string str);
-void	test_one();
-void	test_two();
-
-bool TEST;
 
 
-
-
-int main()
+/**========================================================================
+ *                           PHONEBOOK
+ * for increased portability, checks could be carried out to ensure that the
+ * system is running a Unix-like system before executing the command.
+ * But since this app will run at 42 only, 
+ * we can be sure that the system is Unix-like...
+ *========================================================================**/
+int main(int argc, char **argv)
 {
-	// TEST = true;
-	// std::string c;
-	// while (c != "1" && c != "2")
-	// {
-	// 	std::cout << "test 1 or 2? ";
-	// 	std::cin >> c; 
-	// }
-	// switch (c[0])
-	// {
-	// 	case '1':
-	// 		test_one();
-	// 		break;
-	// 	case '2' :
-	// 		test_two();
-	// 		break;
-	// 	default :
-	// 		print("Fuck off");
-	// 		break;
-	// }
-	test_two();
+	Terminal term;
+
+	if (argc == 1)
+	{
+		std::cout << term.terminalCommand << std::endl;
+		term.execSystemCmd(term.terminalCommand);
+	}
+	else
+		std::cout << "Usage: ./exec" << std::endl;
+	
 	return 0;
-}
-
-void	print(std::string str)
-{
-	if (TEST)
-		std::cout << str << std::endl;
-}
-
-void printB(const std::string& str)
-{
-    // Code d'échappement pour le texte en gras (1) et couleur verte (32)
-	if (TEST)
-    	std::cout << "\033[1;32m" << str << "\033[0m" << std::endl;
-}
-
-void	printF(std::string str)
-{
-		std::cout << str << std::endl;
+	(void)argv;
 }
