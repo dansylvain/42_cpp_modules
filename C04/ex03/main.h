0b19ae4813bb71e00b38a3941b972d4e7f21c8c6
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-03 17:51:27 +0200

    first draft

--- C04/ex03/main.h
+++ C04/ex03/main.h
@@ -23,7 +23,12 @@ typedef enum
 
 void	print(std::string str);
 void	printB(const std::string& str);
-
+void	print(std::string str);
+void	printB(const std::string& str);
+void	printF(std::string str);
+void	printFB(const std::string& str);
+void	test_one();
+void	test_two();
 extern bool TEST;
 
 #endif
