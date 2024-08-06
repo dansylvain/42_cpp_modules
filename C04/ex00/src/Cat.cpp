5099ea0f7fffaf4a316cc2898b762662966de5af
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-06 19:07:18 +0200

    ex00 Makefile refactired

---
+++ C04/ex00/src/Cat.cpp
@@ -0,0 +1,34 @@
+#include "Cat.hpp"
+
+/**========================================================================
+ *                           CONSTRUCTORS AND DESTRUCTOR
+ *========================================================================**/
+Cat::Cat() : Animal() {
+	type = "Cat";
+	print("Cat created");
+}
+
+Cat::Cat(const Cat &other) : Animal(other){
+	print("Cat created");
+	type = other.getType();
+}
+
+Cat::~Cat() {
+	print("Cat destroyed");
+}
+
+/**========================================================================
+ *                           ASSIGNMENT OPERATOR
+ *========================================================================**/
+Cat& Cat::operator=(const Cat& src) {
+	if (this != &src)
+		Animal::operator=(src);
+	return (*this);
+}
+
+/**========================================================================
+ *                           OTHER METHODS
+ *========================================================================**/
+void	Cat::makeSound() const {
+	print(getType() + " says MEOW!");
+}
---
+++ C04/ex00/src/Dog.cpp
@@ -0,0 +1,34 @@
+#include "Dog.hpp"
+
+/**========================================================================
+ *                           CONSTRUCTORS AND DESTRUCTOR
+ *========================================================================**/
+Dog::Dog() : Animal() {
+	type = "Dog";
+	print("Dog created");
+}
+
+Dog::Dog(const Dog &other) : Animal(other) {
+	std::cout << "Dog created" << std::endl;
+	type = other.getType();
+}
+
+Dog::~Dog() {
+	print("Dog destroyed");
+}
+
+/**========================================================================
+ *                           ASSIGNMENT OPERATOR
+ *========================================================================**/
+Dog& Dog::operator=(const Dog& src) {
+	if (this != &src)
+		Animal::operator=(src);
+	return (*this);
+}
+
+/**========================================================================
+ *                           OTHER METHODS
+ *========================================================================**/
+void	Dog::makeSound() const {
+	print(getType() + " says WOOF!");
+}
