ce34f87367ca7826299bf6a95b52f43bb9763dac
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-06 17:25:43 +0200

    input validated

--- C02/ex03/src/main.cpp
+++ C02/ex03/src/main.cpp
@@ -3,13 +3,28 @@
 
 bool bsp( Point const a, Point const b, Point const c, Point const point);
 
-void	readPoint(Point &p)
+void	getInput(std::string str, float *f)
 {
+	bool validInput = false;
+	while (!validInput) {
+		std::cout << str;
+		std::cin >> *f;
+		std::cout << "VALUE GIVEN: " << *f << std::endl;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			std::cout << "Invalid input. Please enter a valid floating point number." << std::endl;
+		} 
+		else
+			validInput = true;
+	}
+}
+
+void readPoint(Point &p) {
 	float x, y;
-	std::cout << "enter x cooridnate: ";
-	std::cin >> x;
-	std::cout << "enter y cooridnate: ";
-	std::cin >> y;
+	getInput("Enter x coordinate: ", &x);
+	getInput("Enter y coordinate: ", &y);
 	p = Point(x, y);
 }
 
@@ -20,18 +35,18 @@ int main(void)
 
 	std::cout << "Hello World !" << std::endl;
 	std::cout << "Enter coordinates for triangle points:" << std::endl;
-    std::cout << "Point 1 (a):" << std::endl;
-    readPoint(a);
-    std::cout << "Point 2 (b):" << std::endl;
-    readPoint(b);
-    std::cout << "Point 3 (c):" << std::endl;
-    readPoint(c);
+	std::cout << "Point 1 (a):" << std::endl;
+	readPoint(a);
+	std::cout << "Point 2 (b):" << std::endl;
+	readPoint(b);
+	std::cout << "Point 3 (c):" << std::endl;
+	readPoint(c);
 	std::cout << "Point to be tested (p):" << std::endl;
-    readPoint(point);
-	std::cout << "triangle point1: "<< a.get_x().toInt() << " : " << a.get_y().toInt() << std::endl;
-	std::cout << "triangle point2: "<< b.get_x().toInt() << " : " << b.get_y().toInt() << std::endl;
-	std::cout << "triangle point3: "<< c.get_x().toInt() << " : " << c.get_y().toInt() << std::endl;
-	std::cout << "point to be tested: "<< point.get_x().toInt() << " : " << point.get_y().toInt() << std::endl;
+	readPoint(point);
+	std::cout << "triangle point1: "	<< a.get_x().toFloat() 		<< " : " << a.get_y().toFloat() << std::endl;
+	std::cout << "triangle point2: "	<< b.get_x().toFloat() 		<< " : " << b.get_y().toFloat() << std::endl;
+	std::cout << "triangle point3: "	<< c.get_x().toFloat() 		<< " : " << c.get_y().toFloat() << std::endl;
+	std::cout << "point to be tested: "	<< point.get_x().toFloat() 	<< " : " << point.get_y().toFloat() << std::endl;
 	
 	bsp(a, b, c, point) ? std::cout << "YES!" : std::cout << "NO!";
 	
