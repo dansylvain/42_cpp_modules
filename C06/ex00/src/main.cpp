71edd480bc3fd6b1dddb949aa7ded82e08ccfe47
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-11 05:26:18 +0200

    advancing

--- C06/ex00/src/main.cpp
+++ C06/ex00/src/main.cpp
@@ -4,14 +4,24 @@
 #include <cctype>
 #include <limits>
 #include <string>
-
-enum PseudoLiterals {
+#include <iomanip>
+enum PseudoLiterals
+{
 	NEG_INFF,
 	POS_INFF,
 	NEG_INF,
 	POS_INF,
 	NAN,
 	NUM_PSEUDO_LITERALS
+};
+
+enum literalTpe
+{
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE,
+	UNKNOWN
 };
 
 const std::string pseudoLiteralsArray[NUM_PSEUDO_LITERALS] = {
@@ -22,26 +32,51 @@ const std::string pseudoLiteralsArray[NUM_PSEUDO_LITERALS] = {
 	"nan"
 };
 
+literalTpe getType(std::string str)
+{
+	literalTpe type;
 
+	if (ScalarConverter::isInt(str))
+		type = INT;
+	else if (ScalarConverter::isChar(str))
+		type = CHAR;
+	else if (ScalarConverter::isFloat(str))
+		type = FLOAT;
+	else if (ScalarConverter::isDouble(str))
+		type = DOUBLE;
+	else
+		type = UNKNOWN;
+	return (type);
+}
 
-int main() {
+int main()
+{
 	std::string input;
+	literalTpe type;
 
 	while (1)
 	{
-			std::cin >> input;
-			if (ScalarConverter::isInt(input)) {
-				std::cout << "Int" << std::endl;
-			} else if (ScalarConverter::isChar(input)) {
-				std::cout << "Char" << std::endl;
-			} else if (ScalarConverter::isFloat(input)) {
-				std::cout << "Float" << std::endl;
-			} else if (ScalarConverter::isDouble(input)) {
-				std::cout << "Double" << std::endl;
-			} else {
-				std::cout << "Unknown type" << std::endl;
-			}
+		std::cin >> input;
+		type = getType(input);
+		std::istringstream issInt(input);
+		double Value;
+		issInt >> Value;
+
+		if (type == CHAR)
+		{
+			std::cout << input[0] << std::endl;	
+			Value = input[0];
+		}
+		std::cout << "float: " << std::fixed << std::setprecision(2) <<  static_cast<float>(Value) << "f" << std::endl;
+		std::cout << "int: " <<  static_cast<int>(Value) << std::endl;
+		std::cout << "char: " <<  static_cast<char>(Value) << std::endl;
+		std::cout << "double: " <<  (Value) << std::endl;
+
+		// std::cout << type << std::endl;
 	}
 
-	return 0;
+	return (0);
 }
+
+
+
