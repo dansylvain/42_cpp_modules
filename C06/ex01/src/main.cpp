80e2ea3955597ce04ab6b1dd92c0e84e28772801
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-11 16:08:22 +0200

    file uncommented

--- C06/ex01/src/main.cpp
+++ C06/ex01/src/main.cpp
@@ -1,49 +1,44 @@
 #include <iostream>
-#include <stdint.h> // Pour uintptr_t
+#include <stdint.h>
 
-// Définition de la structure Data
-struct Data {
-    int number;
-    std::string text;
+struct Data
+{
+	int number;
+	std::string text;
 
-    Data(int num, const std::string& str) : number(num), text(str) {}
+	Data(int num, const std::string& str) : number(num), text(str) {}
 };
 
-// Définition de la classe Serializer
-class Serializer {
+class Serializer
+{
 public:
-    static uintptr_t serialize(Data* ptr){
-        return reinterpret_cast<uintptr_t>(ptr);
-    }
-
-    static Data* deserialize(uintptr_t raw) {
-        return reinterpret_cast<Data*>(raw);
-    }
+	static uintptr_t serialize(Data* ptr)
+	{
+		return reinterpret_cast<uintptr_t>(ptr);
+	}
+	static Data* deserialize(uintptr_t raw)
+	{
+		return reinterpret_cast<Data*>(raw);
+	}
 
 private:
-    // Pour éviter que la classe soit instanciée
-    Serializer() {}
-    ~Serializer() {}
+	Serializer() {}
+	~Serializer() {}
 };
 
 int main() {
-    // Création d'un objet Data
-    Data originalData(42, "Hello World");
-
-    // Sérialisation de l'objet
-    uintptr_t serializedData = Serializer::serialize(&originalData);
-
-    // Désérialisation de l'objet
-    Data* deserializedData = Serializer::deserialize(serializedData);
-
-    // Vérification que les données ont été correctement désérialisées
-    if (&originalData == deserializedData) {
-        std::cout << "Success: The original pointer and the deserialized pointer are the same.\n";
-        std::cout << "Number: " << deserializedData->number << "\n";
-        std::cout << "Text: " << deserializedData->text << "\n";
-    } else {
-        std::cout << "Failure: The pointers are different.\n";
-    }
-
-    return 0;
+	Data originalData(42, "Hello World");
+	uintptr_t serializedData = Serializer::serialize(&originalData);
+	Data* deserializedData = Serializer::deserialize(serializedData);
+	if (&originalData == deserializedData)
+	{
+		std::cout << "Success: The original pointer and the deserialized pointer are the same.\n";
+		std::cout << "Number: " << deserializedData->number << "\n";
+		std::cout << "Text: " << deserializedData->text << "\n";
+	}
+	else
+	{
+		std::cout << "Failure: The pointers are different.\n";
+	}
+	return 0;
 }
