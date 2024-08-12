5aea2ce08dc8711e6e28b707b82c517139c2161a
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-12 17:32:11 +0200

    updated test main

--- C07/ex00/src/main.cpp
+++ C07/ex00/src/main.cpp
@@ -1,12 +1,24 @@
 #include "templateFunctions.hpp"
 
+void	pause()
+{
+	std::cout << std::endl;
+	std::cout << "Press Enter to continue..." << std::endl;
+	std::cin.get();
+}
+
+void	printB(std::string str)
+{
+	std::cout << "\033[31;1m" << str << "\033[0m" << std::endl;
+}
+
 int main()
 {
 	{
 		int a = 2;
 		int b = 3;
 		::swap( a, b );
-		std::cout << "Tests from subject's main" << std::endl;
+		printB("Tests from subject's main");
 		std::cout << "a = " << a << ", b = " << b << std::endl;
 		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
@@ -17,9 +29,9 @@ int main()
 		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	}
-	std::cout << std::endl;
+	pause();
 	{
-		std::cout << "Test with type <int>" << std::endl;
+		printB("Test with type <int>");
 		int x = 10, y = 20;
 		std::cout << "before swap: x = " << x << ", y = " << y << std::endl;
 		swap(x, y);
@@ -28,9 +40,9 @@ int main()
 		std::cout << "Min of 10 and 20: " << min(x, y) << std::endl;
 		std::cout << "Max of 10 and 20: " << max(x, y) << std::endl;
 	}
-	std::cout << std::endl;
+	pause();
 	{
-		std::cout << "Test with type <double>" << std::endl;
+		printB("Test with type <double>");
 		double x = 15.5, y = 10.5;
 		std::cout << "before swap: x = " << x << ", y = " << y << std::endl;
 		swap(x, y);
@@ -38,9 +50,9 @@ int main()
 		std::cout << "Min of 15.5 and 10.5: " << min(x, y) << std::endl;
 		std::cout << "Max of 15.5 and 10.5: " << max(x, y) << std::endl;
 	}
-	std::cout << std::endl;
+	pause();
 	{
-		std::cout << "Test with type <char>" << std::endl;
+		printB("Test with type <char>");
 		char a = 'A', b = 'B';
 		std::cout << "before swap: a = " << a << ", b = " << b << std::endl;
 		swap(a, b);
