767c8599b5b35241d93a7ddcf249a05a1da0210d
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-12 11:24:29 +0200

    refactored c00

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -20,6 +20,7 @@ int main(int argc, char **argv)
 		for (int i = 1; i < argc; i++)
 			megaphone.shout(argv[i]);
 	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
 	return 0;
 }
