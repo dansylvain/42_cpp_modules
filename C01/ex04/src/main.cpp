29b242ae3b96a410745a10bbdb1f6bfd4ee84252
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-13 13:52:03 +0200

    updated replace

--- C01/ex04/src/main.cpp
+++ C01/ex04/src/main.cpp
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dansylvain <dansylvain@student.42.fr>      +#+  +:+       +#+        */
+/*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 10:57:01 by dansylvain        #+#    #+#             */
-/*   Updated: 2024/07/20 11:10:19 by dansylvain       ###   ########.fr       */
+/*   Updated: 2024/08/13 13:51:29 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Utils.hpp"
+#include <string>
+#include <cstdlib>
 
 int main( int argc, char **argv )
 {
@@ -23,5 +25,12 @@ int main( int argc, char **argv )
 		return (2);
 	Utils::createNewFile(inputFile, outputFile, argv);
 	Utils::closeStreams(inputFile, outputFile);
+	
+	// display output
+	std::cout << "Input file: " << std::endl;
+	system((std::string("cat ") + argv[1]).c_str());	
+	std::cout << std::endl;
+	std::cout << "Output file: " << std::endl;
+	system((std::string("cat ") + argv[1] + ".replace").c_str());	
 	return (0);
 }
