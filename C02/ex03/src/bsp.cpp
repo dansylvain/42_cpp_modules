c6222a88649af78987a8cbc81340f51a0c6bf788
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-14 13:55:29 +0200

    bsp fixed

--- C02/ex03/src/bsp.cpp
+++ C02/ex03/src/bsp.cpp
@@ -2,21 +2,21 @@
 #include "Fixed.hpp"
 #include <cstdio>
 
-bool haveSameSign(int x, int y);
-int	cross_product_2d(int x[], int y[], int z[]);
+bool haveSameSign(float x, float y);
+float cross_product_2d(float x[], float y[], float z[]);
 void	initializeArray(int array[], Point const p);
 
-int	cross_product_2d(int x[], int y[], int z[]){
+float	cross_product_2d(float x[], float y[], float z[]){
 	return ((y[0]-x[0])*(z[1]-x[1])-(y[1]-x[1])*(z[0]-x[0]));
 }
 
-void	initializeArray(int array[], Point const p)
+void	initializeArray(float array[], Point const p)
 {
-	array[0] = p.get_x().toInt();
-	array[1] = p.get_y().toInt();
+	array[0] = p.get_x().toFloat();
+	array[1] = p.get_y().toFloat();
 }
 
-bool haveSameSign(int x, int y)
+bool haveSameSign(float x, float y)
 {
 	if ((x > 0 && y > 0) || (x < 0 && y < 0))
 		return (true);
@@ -30,17 +30,17 @@ bool haveSameSign(int x, int y)
  *========================================================================**/
 bool bsp( Point const a, Point const b, Point const c, Point const point){
 	
-	int A[2], B[2], C[2], P[2];
+	float A[2], B[2], C[2], P[2];
 	
 	initializeArray(A, a);
 	initializeArray(B, b);
 	initializeArray(C, c);
 	initializeArray(P, point);
 
-	int cp1 = cross_product_2d(A, B, C);
-	int cp2 = cross_product_2d(A, B, P);
-	int cp3 = cross_product_2d(B, C, P);
-	int cp4 = cross_product_2d(C, A, P);
+	float cp1 = cross_product_2d(A, B, C);
+	float cp2 = cross_product_2d(A, B, P);
+	float cp3 = cross_product_2d(B, C, P);
+	float cp4 = cross_product_2d(C, A, P);
 
 	bool sameSign1 = haveSameSign(cp1, cp2);
 	bool sameSign2 = haveSameSign(cp1, cp3);
--- C02/ex03/src/main.cpp
+++ C02/ex03/src/main.cpp
@@ -33,32 +33,94 @@ void	print(std::string str)
 }
 
 
-int main(void)
-{
-	Point a(0,0), b(10,0), c(0,10), point(1,2);
+// int main(void)
+// {
+// 	Point a(0,0), b(10,0), c(0,10), point(1,2);
 
-	print("Welcome to the Jungle !");
-	print("");
-	print("");
-	print("Enter coordinates for triangle points:");
-	print("");
-	print("Point 1 (a):");
-	readPoint(a);
-	print("");
-	print("Point 2 (b):");
-	readPoint(b);
-	print("");
-	print("Point 3 (c):");
-	readPoint(c);
-	print("");
-	print("Point to be tested (p):");
-	readPoint(point);
-	print("");
-	std::cout << "triangle point1: x: "	<< a.get_x().toFloat() 		<< " , y: " << a.get_y().toFloat() << std::endl;
-	std::cout << "triangle point2: x: "	<< b.get_x().toFloat() 		<< " , y: " << b.get_y().toFloat() << std::endl;
-	std::cout << "triangle point3: x: "	<< c.get_x().toFloat() 		<< " , y: " << c.get_y().toFloat() << std::endl;
-	std::cout << "point to be tested: x: "	<< point.get_x().toFloat() 	<< " , y: " << point.get_y().toFloat() << std::endl;
-	print("");
-	bsp(a, b, c, point) ? print("YES!") : print("NO!");
-	return (0);
+// 	print("Welcome to the Jungle !");
+// 	print("");
+// 	print("");
+// 	print("Enter coordinates for triangle points:");
+// 	print("");
+// 	print("Point 1 (a):");
+// 	readPoint(a);
+// 	print("");
+// 	print("Point 2 (b):");
+// 	readPoint(b);
+// 	print("");
+// 	print("Point 3 (c):");
+// 	readPoint(c);
+// 	print("");
+// 	print("Point to be tested (p):");
+// 	readPoint(point);
+// 	print("");
+// 	std::cout << "triangle point1: x: "	<< a.get_x().toFloat() 		<< " , y: " << a.get_y().toFloat() << std::endl;
+// 	std::cout << "triangle point2: x: "	<< b.get_x().toFloat() 		<< " , y: " << b.get_y().toFloat() << std::endl;
+// 	std::cout << "triangle point3: x: "	<< c.get_x().toFloat() 		<< " , y: " << c.get_y().toFloat() << std::endl;
+// 	std::cout << "point to be tested: x: "	<< point.get_x().toFloat() 	<< " , y: " << point.get_y().toFloat() << std::endl;
+// 	print("");
+// 	bsp(a, b, c, point) ? print("YES!") : print("NO!");
+// 	return (0);
+// }
+
+
+void	fire( Point const a, Point const b, Point const c, Point const point )
+{	
+	// std::cout << "point= " << point << std::endl;
+	if (bsp(a, b, c, point))
+		std::cout << "\033[32m 👌 You are in the triangle mg !\033[0m"
+			<< std::endl << std::endl;
+	else
+		std::cout << "\033[33m ✍️ try again !\033[0m"
+			<< std::endl << std::endl;
+}
+
+int main( void )
+{
+	Point a(2, 1);
+	Point b(6, 4);
+	Point c(8, 1);
+	
+	// std::cout << "a= " << a << std::endl;
+	// std::cout << "b= " << b << std::endl;
+	// std::cout << "c= " << c << std::endl
+	// 	<< std::endl;
+		
+	{		
+		Point point(5, 2.5);
+		fire(a, b, c, point);
+	}
+	{
+		Point point(7, 2);
+		fire(a, b, c, point);
+	}
+	{
+		Point point(7, 0);
+		fire(a, b, c, point);
+	}
+	{
+		Point point(7, 1);
+		fire(a, b, c, point);
+	}
+	{
+		Point point(7, 1.01);
+		fire(a, b, c, point);
+	}
+	{
+		Point point(3, 3);
+		fire(a, b, c, point);
+	}
+	{
+		Point point(-3, -3);
+		fire(a, b, c, point);
+	}
+	{
+		Point point(2, 1);
+		fire(a, b, c, point);
+	}
+	{
+		Point point(2.1, 1.01);
+		fire(a, b, c, point);
+	}
+	return 0;
 }
