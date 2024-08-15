79697b828d24b378dd8d671a3c16689898c77e70
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-15 09:44:17 +0200

    ex02 done

--- C07/ex02/src/main.cpp
+++ C07/ex02/src/main.cpp
@@ -12,21 +12,21 @@ int main()
 	Array<int> emptyArray;
 	std::cout << "Empty array size: " << emptyArray.size() << std::endl;
 
-	// pause();
-	printB("\nConstruction with an unsigned int n as a parameter: Creates an array of n elements initialized by default:");
+	pause();
+	printB("Construction with an unsigned int n as a parameter: Creates an array of n elements initialized by default:");
 	Array<int> intArray(5);
 	printArray(intArray);
 
-	// pause();
+	pause();
 	for (unsigned int i = 0; i < intArray.size(); ++i)
 		intArray[i] = static_cast<int>(i * 10);
-	printB("\nAfter putting in some values:");
+	printB("After putting in some values:");
 	printArray(intArray);
 
 	printB("\nUse of subscript operator [] :");
 	std::cout << "value of intArray[2]: " << intArray[2] << std::endl;
-	// pause();
-	printB("\nTest accessing out of bound value:");
+	pause();
+	printB("Test accessing out of bound value:");
 	try
 	{
 		std::cout << intArray[10] << std::endl;
@@ -35,34 +35,37 @@ int main()
 	{
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
+	
+	pause();
+	printB("Test constructor bu copy");
+	print("\nCreate arrayOne (size 5)");
 	Array<int> arrayOne(5);
 	printArray(arrayOne);
+	print("\nCreate arrayTwo (copy of arrayOne)");
+	Array<int>arrayTwo(arrayOne);
+	printArray(arrayTwo);
+	print("\nModify arrayOne content and print both arrays");
+	arrayOne[0] = 21;
+	arrayOne[1] = 42;
+	arrayOne[2] = 666;
+	arrayOne[3] = 300;
+	arrayOne[4] = 2001;
+	printArray(arrayOne);
+	printArray(arrayTwo);
+	pause();
+	printB("Test assignment operator");
+	arrayTwo = arrayOne;
+	printArray(arrayOne);
+	printArray(arrayTwo);
 	
-	
-	// pause();
-	printB("\nTest constructor bu copy");
-		print("Create arrayOne (size 5)");
-
-		// print("Create arrayTwo (copy of arrayOne)");
-		// Array<int>arrayTwo(arrayOne);
-		// printArray(arrayTwo);
-
-
 	return 0;
 }
-
-// template <typename T>
-// void printArray(const Array<T>& array) {
-//     for (unsigned int i = 0; i < array.size(); ++i) {
-//         std::cout << "array[" << i << "] = " << array[i] << std::endl;
-//     }
-// }
 
 template <typename T>
 void	printArray(const Array<T>& array)
 {
 	for (unsigned int i = 0; i < array.size(); ++i)
-		std::cout << "intArray[" << i << "] = " << array[i] << std::endl;
+		std::cout << "Array[" << i << "] = " << array[i] << std::endl;
 }
 
 void	print(std::string str)
