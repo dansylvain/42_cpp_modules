2e2ff39c953b295c5935afe3ab65c9d5fdeeea0e
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-25 09:37:04 +0200

    input taken

--- C06/ex00/inc/ScalarConverter.hpp
+++ C06/ex00/inc/ScalarConverter.hpp
@@ -5,11 +5,20 @@ class ScalarConverter
 {
 	private:
 
+		// coplien
 		ScalarConverter(const ScalarConverter& other);
 		virtual const ScalarConverter& operator=(const ScalarConverter& rhs) = 0;
 	public:
+		// coplien
 		ScalarConverter();
 		~ScalarConverter();
 
+		// isType methods
+		static bool	isChar(const std::string &str);
+		static bool	isInt(const std::string &str);
+		static bool	isFloat(const std::string &str);
+		static bool	isDouble(const std::string &str);
+		
+		// other methods
 		static void	convert(const std::string& str);
 };
--- C06/ex00/src/ScalarConverter.cpp
+++ C06/ex00/src/ScalarConverter.cpp
@@ -1,5 +1,9 @@
 #include "ScalarConverter.hpp"
+#include <sstream>
 
+/**========================================================================
+ *                           COPLIEN
+ *========================================================================**/
 ScalarConverter::ScalarConverter()
 {
 	
@@ -21,7 +25,62 @@ ScalarConverter::~ScalarConverter()
 {
 }
 
-void	ScalarConverter::convert(const std::string& str)
+/**========================================================================
+ *                           ISTYPE METHODS
+//? ISFLOAT && ISDOUBLE
+//? substring created to check validity of float before suffix 'f'
+//! missing digits before or after the point is considered valid input
+//! ex: 42.f and .42f are valid floats
+*========================================================================**/
+bool ScalarConverter::isChar(const std::string &str)
 {
-	std::cout << "convert: " << str << std::endl;
+	return (str.length() == 3 && str[0] == '\'' && str[2] == '\'');
+}
+
+bool ScalarConverter::isInt(const std::string &str)
+{
+	std::istringstream iss(str);
+	int intValue;
+	return !(iss >> intValue).fail() && iss.eof();
+}
+
+bool ScalarConverter::isFloat(const std::string &str)
+{
+	if (str[str.length() - 1] != 'f' || str[0] == '.' || str[str.length() - 2] == '.')
+		return false;
+	std::string numberPart = str.substr(0, str.size() - 1);
+	std::istringstream iss(numberPart);
+	float floatValue;
+	iss >> floatValue;
+	return !iss.fail() && iss.eof() && (numberPart.find('.') != std::string::npos);
+}
+
+bool ScalarConverter::isDouble(const std::string &str)
+{
+	if (str[0] == '.' || str[str.length() - 1] == '.')
+		return (0);
+	std::istringstream iss(str);
+	double doubleValue;
+	return !(iss >> doubleValue).fail() && iss.eof() && (str.find('.') != std::string::npos);
+}
+
+/**========================================================================
+ *                           OTHER METHODS
+ *========================================================================**/
+void ScalarConverter::convert(const std::string& str)
+{
+	std::string input;
+
+	while (true)
+	{
+		std::cout << "input: " << std::flush;
+		std::getline(std::cin, input);
+		if (input.empty())
+		{
+			std::cout << "Input cannot be empty. Please enter a valid input." << std::endl;
+			continue ;
+		}
+		std::cout << str << ": " << input << std::endl;
+		input.clear();
+	}
 }
