e82f632c273625505db63b408c452c04544aa8e5
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-25 08:19:46 +0200

    restart ex00-C06

--- C06/ex00/src/main.cpp
+++ C06/ex00/src/main.cpp
@@ -1,24 +1,7 @@
-#include "ScalarConverter.hpp"
+#include "main.hpp"
 
-/**========================================================================
- *                             THE EXERCISE SHOULD BE REDONE
- *  in fact, I have to first detect the type of the literal passed 
- * as parameter, convert it from string to its actual type, then convert
- * it explicitly to the three other data types. Lastly, display 
- * the results as shown below.
- * =======================================================================**/
-int main() {
-	std::string input;
-
-	while (true) {
-		std::cout << "Enter a string representation of a C++ literal in its most common form (or type 'exit' to quit): " << std::flush;
-		std::getline(std::cin, input);
-		if (input == "exit")
-			break;
-		if (input.empty())
-			continue;
-		ScalarConverter::convert(input);
-	}
-
-	return 0;
+int main(void)
+{
+    std::cout << "Welcome to the Jungle" << std::endl;
+    return (0);
 }
