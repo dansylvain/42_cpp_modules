7b827ba7c87457bdc83bca0f98e07a29b850753a
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-25 17:57:15 +0200

    ex01 done

--- C06/ex01/src/main.cpp
+++ C06/ex01/src/main.cpp
@@ -1,16 +1,38 @@
 #include "main.hpp"
 
-int main() {
-	Data originalData(42, "Hello World");
-	uintptr_t serializedData = Serializer::serialize(&originalData);
-	Data* deserializedData = Serializer::deserialize(serializedData);
-	if (&originalData == deserializedData)
+void	compareDataBeforeAndAfterSerialization(Data *originalData, Data *deserializedData)
+{
+	if (originalData == deserializedData)
 	{
 		std::cout << "Success: The original pointer and the deserialized pointer are the same.\n";
 		std::cout << "Number: " << deserializedData->number << "\n";
-		std::cout << "Text: " << deserializedData->text << "\n";
+		std::cout << "Text: " << "\"" << deserializedData->text << "\"" << "\n";
 	}
 	else
 		std::cout << "Failure: The pointers are different.\n";
+}
+
+int main()
+{
+	{
+		Data originalData(42, "Hello World");
+		uintptr_t serializedData = Serializer::serialize(&originalData);
+		Data* deserializedData = Serializer::deserialize(serializedData);
+		compareDataBeforeAndAfterSerialization(&originalData, deserializedData);
+	}
+	std::cout << std::endl;
+	{
+		Data originalData(0, "");
+		uintptr_t serializedData = Serializer::serialize(&originalData);
+		Data* deserializedData = Serializer::deserialize(serializedData);
+		compareDataBeforeAndAfterSerialization(&originalData, deserializedData);
+	}
+	std::cout << std::endl;
+	{
+		Data originalData(-42, "niania");
+		uintptr_t serializedData = Serializer::serialize(&originalData);
+		Data* deserializedData = Serializer::deserialize(serializedData);
+		compareDataBeforeAndAfterSerialization(&originalData, deserializedData);
+	}
 	return 0;
 }
