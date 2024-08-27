9cd9d39859472c5560fc76e502d104c0a9fab7a2
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-27 16:14:25 +0200

    reorganized files

--- C07/ex01/inc/main.hpp
+++ C07/ex01/inc/main.hpp
@@ -2,3 +2,9 @@
 
 #include "iter.hpp"	
 #include "print.hpp"
+
+void	printInt(int& value);
+void	printB(std::string str);
+void	printChar(char& value);
+void	printString(std::string& value);
+void	pause();
