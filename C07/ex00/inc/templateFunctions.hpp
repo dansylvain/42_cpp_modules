4af81894fd45d0ae1ab9a17be023352999ea1b9c
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-28 13:28:46 +0200

    added namespace for complex type support

--- C07/ex00/inc/templateFunctions.hpp
+++ C07/ex00/inc/templateFunctions.hpp
@@ -1,24 +1,29 @@
 #pragma once
 #include <iostream>
 
-template <typename T>
-void swap(T& a, T& b)
+//? namespace created in order to avoid function name conflict (min && max)
+namespace myNameSpace
 {
-	T temp = a;
-	a = b;
-	b = temp;
-}
+	template <typename T>
+	void swap(T& a, T& b)
+	{
+		T temp = a;
+		a = b;
+		b = temp;
+	}
 
-template <typename T>
-T min(const T& a, const T& b)
-{
-	return (a < b) ? a : b;
-}
+	template <typename T>
+	T min(const T& a, const T& b)
+	{
+		return (a < b) ? a : b;
+	}
 
-template <typename T>
-T max(const T& a, const T& b)
-{
-	return (a >= b) ? a : b;
+	template <typename T>
+	T max(const T& a, const T& b)
+	{
+		return (a > b) ? a : b;
+	}
+
 }
 
 void	pause();
--- C07/ex00/src/main.cpp
+++ C07/ex00/src/main.cpp
@@ -5,17 +5,17 @@ int main()
 	{
 		int a = 2;
 		int b = 3;
-		::swap( a, b );
+		myNameSpace::swap( a, b );
 		printB("Tests from subject's main");
 		std::cout << "a = " << a << ", b = " << b << std::endl;
-		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::cout << "min( a, b ) = " << myNameSpace::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << myNameSpace::max( a, b ) << std::endl;
 		std::string c = "chaine1";
 		std::string d = "chaine2";
-		::swap(c, d);
+		myNameSpace::swap(c, d);
 		std::cout << "c = " << c << ", d = " << d << std::endl;
-		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		std::cout << "min( c, d ) = " << myNameSpace::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << myNameSpace::max( c, d ) << std::endl;
 	}
 	pause();
 	{
@@ -23,11 +23,11 @@ int main()
 		int x = 10, y = 20;
 		std::cout << "x = " << x << ", y = " << y << std::endl;
 		std::cout << "before swap: x = " << x << ", y = " << y << std::endl;
-		swap(x, y);
+		myNameSpace::swap(x, y);
 		std::cout << "After  swap: x = " << x << ", y = " << y << std::endl;
 		
-		std::cout << "Min of 10 and 20: " << min(x, y) << std::endl;
-		std::cout << "Max of 10 and 20: " << max(x, y) << std::endl;
+		std::cout << "Min of 10 and 20: " << myNameSpace::min(x, y) << std::endl;
+		std::cout << "Max of 10 and 20: " << myNameSpace::max(x, y) << std::endl;
 	}
 	pause();
 	{
@@ -35,22 +35,22 @@ int main()
 		double x = 15.5, y = 10.5;
 		std::cout << "x = " << x << ", y = " << y << std::endl;
 		std::cout << "before swap: x = " << x << ", y = " << y << std::endl;
-		swap(x, y);
+		myNameSpace::swap(x, y);
 		std::cout << "After  swap: x = " << x << ", y = " << y << std::endl;
-		std::cout << "Min of 15.5 and 10.5: " << min(x, y) << std::endl;
-		std::cout << "Max of 15.5 and 10.5: " << max(x, y) << std::endl;
+		std::cout << "Min of 15.5 and 10.5: " << myNameSpace::min(x, y) << std::endl;
+		std::cout << "Max of 15.5 and 10.5: " << myNameSpace::max(x, y) << std::endl;
 	}
 	pause();
 	{
 		printB("Test with type <char>");
-		char a = 'A', b = 'A';
+		std::string a = "Hello world", b = "Welcome to the Jungle";
 		std::cout << "a = " << a << ", b = " << b << std::endl;
 		std::cout << "before swap: a = " << a << ", b = " << b << std::endl;
-		swap(a, b);
+		myNameSpace::swap(a, b);
 		std::cout << "After  swap: a = " << a << ", b = " << b << std::endl;
 		
-		std::cout << "Min of a and b: " << min(a, b) << std::endl;
-		std::cout << "Max of a and b: " << max(a, b) << std::endl;
+		std::cout << "Min of a and b: " << myNameSpace::min(a, b) << std::endl;
+		std::cout << "Max of a and b: " << myNameSpace::max(a, b) << std::endl;
 
 	}
 	return 0;
