717618ae3a1986a8579e9187cbaa3a0e6ae4dcf6
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-28 07:21:05 +0200

    created files

---
+++ C08/ex02/inc/main.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void	print(std::string str);
+void	printB(std::string str);
+void	myPause();
+void	printTab(std::vector<int> tab);
+void	saveVectorToFile(std::vector<int> tab, std::string filename);
---
+++ C08/ex02/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Welcome to the Jungle" << std::endl;
+	return (0);
+}
