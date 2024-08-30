cbe9edcf053ad331db9da34199b832fe9e06d162
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-30 16:44:36 +0200

    arg number secu added

--- C09/ex01/src/main.cpp
+++ C09/ex01/src/main.cpp
@@ -1,6 +1,10 @@
 #include "main.hpp"
-int main()
+int main(int argc, char** argv)
 {
-	std::cout << "Welcome to the jungle" << std::endl;
+	if (argc != 2)
+		return (std::cout << "Wrong arg number" << std::endl, 0);
+	std::cout << "Welcome to the jungle: " << argv[1] << std::endl;
+
+
 	return (0);
 }
