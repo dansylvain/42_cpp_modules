04ef8b5c58ae94176434c1fdc5762287b6eeba7d
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-31 08:38:50 +0200

    files created

---
+++ C09/ex02/inc/main.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+
+void	print(std::string str);
+void	printB(std::string str);
+void	pause();
---
+++ C09/ex02/src/main.cpp
@@ -0,0 +1,7 @@
+#include "main.hpp"
+
+int main()
+{
+	print("Welcome to the jungle");
+	return(0);
+}
