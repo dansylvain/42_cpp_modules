f8c94d906011bf6abdaa3132c7b69ae1dd0acbb8
Dan SYLVAIN <dansylvain84@gmail.com>
2024-08-31 16:30:57 +0200

    added deque funcs

--- C09/ex02/src/main.cpp
+++ C09/ex02/src/main.cpp
@@ -1,11 +1,6 @@
 #include "main.hpp"
 #include <PmergeMe.hpp>
-	// parsing
 	
-	// merge-insert sorting algorithm (Ford-Johnson algorithm)
-	
-	// use containers std::vector and std::deque
-
 	// tests to be made with at least 3000 different integers
 
 	/* EXEMPLE OUTPUT:
@@ -16,8 +11,6 @@
 	Time to process a range of 5 elements with std::[..] : 0.00031 us
 	Time to process a range of 5 elements with std::[..] : 0.00014 us
 	*/
-
-	// The management of errors related to duplicates is left to your discretion. => ???
 
 int *convertInput(int argc, char** argv, int *intCount)
 {
@@ -38,21 +31,26 @@ int main(int argc, char **argv)
 {
 	int intCount;
 	PmergeMe PmergeMe;
-	// 1: associate values by pairs + set odd/even boolean
 	int *input = convertInput(argc, argv, &intCount);
-	// 2: input validation (negativ numbers, non ints)
+
+	//! COMMON FUNCS
 	PmergeMe.validateInput(input);
-	// 3: create Jacobstahl suite of index
 	PmergeMe.createJacobstahlSequence(intCount);
-	// 4: pre-sort pairs (lower/higher value)
+
+	//! VECTOR FUNCS
 	PmergeMe.createFirstSortedPairing(input, PmergeMe._vector);
-	// 5: sort pairs (by higher value, <= 20 values => insert sort / >20 values => merge sort)
 	PmergeMe.sortPairsByMainChainHighestValue(PmergeMe._vector);
-	// 6: insert values through binary search according to Jacobstahl suite
 	PmergeMe.insertPendantValuesThroughBinarySearch(PmergeMe._vector);
-	// 7: IF ODD: insert last value
 	PmergeMe.insertStraggler(PmergeMe._vector, input[intCount]);
 	PmergeMe.displayResults(PmergeMe._vector);
+
+	//! DEQUE FUNCS
+	PmergeMe.createFirstSortedPairing(input, PmergeMe._deque);
+	PmergeMe.sortPairsByMainChainHighestValue(PmergeMe._deque);
+	PmergeMe.insertPendantValuesThroughBinarySearch(PmergeMe._deque);
+	PmergeMe.insertStraggler(PmergeMe._deque, input[intCount]);
+	PmergeMe.displayResults(PmergeMe._deque);
+	
 	cleanRessources(input);
 	print("Welcome to the jungle");
 	return(0);
